90161556572ffcf2bdd13eca7c52010d467af7b4
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2023-05-28 06:30:53 +0530

    fixes - maker.py

--- src/f-dataframe_maker/maker.py
+++ src/f-dataframe_maker/maker.py
@@ -13,13 +13,13 @@ for df_link in os.listdir(source_dir):
     df = pd.read_csv(df_full_source, 
                      sep='|', 
                      on_bad_lines='skip') # only around 20 bad rows, can be ignored
-    if df.shape[1] != 3:   # Enforce every row has three columns
+    if df.shape[1] != 5:   # Enforce every row has three columns
         continue
     df = df.iloc[:, 1:-1]  # remove blank unused columns
     df.columns = ['English Word', 'Part of Speech', 'Odia Translation']
     dataframes.append(df)
     print(df.shape)
-    
+
 
 data = pd.concat(dataframes, axis=0).reset_index(drop=True)
 print(data.shape)
@@ -30,5 +30,5 @@ rows_to_remove = data[(data[data.columns[0]].str.contains('-')) & (data[data.col
 data = data.drop(rows_to_remove)
 
 # save the dataframe
-full_output_location = os.path.join(source_dir, output_name, index=None)
+full_output_location = os.path.join(output_dir, output_name)
 data.to_csv(full_output_location, index=None)
